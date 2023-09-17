a1a4162d195430f3fd440f7291c1912bc2e4c04b
Ahzam Akhtar <ahzam8186@gmail.com>
2023-09-17 18:06:34 +0530

    UI v2

--- components/micro/Games.js
+++ components/micro/Games.js
@@ -5,7 +5,7 @@ const Games = () => {
   const Data = [
     {
       name: "Space Invaders",
-      img: "/spaceinvaders.png"
+      img: "/spacenew.webp"
     },
     {
       name: "Coin Flip",
@@ -32,14 +32,24 @@ const Games = () => {
           {Data.map((item) => {
             return (
               <>
-                <div>
-                  <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-                    <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src={item.img} alt="" />
-                    <div class="flex flex-col justify-between p-4 leading-normal">
-                      <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{item.name}</h5>
+                <div className='flex flex-col'>
+                  <div>
+                    <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                      <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src={item.img} alt="" />
+                      <div class="flex flex-col justify-between p-4 leading-normal">
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{item.name}</h5>
 
-                    </div>
-                  </a>
+                      </div>
+                    </a>
+                  </div>
+                  <div className='mt-5'>
+
+                    <a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                      <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                      <p class="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                    </a>
+
+                  </div>
                 </div>
               </>
             )
