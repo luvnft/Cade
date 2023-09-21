c2951de1fc4a59fb9d0d11159019b7308a022d78
Ahzam Akhtar <ahzam8186@gmail.com>
2023-09-22 03:23:16 +0530

    trophies ui update

--- pages/Profile/[slug].js
+++ pages/Profile/[slug].js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { GiTwoCoins } from 'react-icons/gi'
-import {BsTrophyFill} from 'react-icons/bs'
+import { BsTrophyFill } from 'react-icons/bs'
 const Profile = () => {
     return (
         <>
-            <div className='flex flex-row'>
+        <div className="bg-[url('/kn.jpg')]">
+            <div className='flex flex-row bg-'>
 
 
                 <section class="text-gray-600 body-font lg:px-28">
@@ -18,7 +19,7 @@ const Profile = () => {
                                         </div>
                                         <div class="flex-grow">
                                             <h2 class="text-white text-2xl title-font font-medium mb-3">UserName</h2>
-                                            
+
                                             <div className='flex flex-row'>
                                                 <GiTwoCoins className='text-4xl text-yellow-400' />
                                                 <h1 className='ml-3 text-xl text-white'>100 000 CADE</h1>
@@ -38,7 +39,8 @@ const Profile = () => {
 
                                 <div class="flex rounded-lg  border-opacity-50 p-8 sm:flex-row flex-col">
                                     <div class="w-16 h-16 sm:mr-8 sm:mb-0 mb-4 inline-flex items-center justify-center rounded-full  flex-shrink-0">
-                                        <BsTrophyFill className='text-4xl text-yellow-400'/>
+                                        {/* <BsTrophyFill className='text-7xl mt-5 text-red-500' /> */}
+                                        <img height={100}  width={100} className='mt-10'  src='/trophy.png' alt='alt'/>
                                     </div>
                                     <div class="flex-grow">
                                         <h2 class="text-white text-2xl title-font font-medium mb-3">Your Summary</h2>
@@ -70,76 +72,54 @@ const Profile = () => {
 
             </div>
 
+
+
             <div class="h-px w-full bg-gray-400"></div>
 
-            <section class="text-gray-600 body-font overflow-hidden">
+            <section class="text-gray-600 body-font">
                 <div class="container px-5 py-24 mx-auto">
-                    <div class="flex flex-wrap -m-12">
-                        <div class="p-12 md:w-1/2 flex flex-col items-start">
-                            <span class="inline-block py-1 px-2 rounded bg-indigo-50 text-indigo-500 text-xs font-medium tracking-widest">CATEGORY</span>
-                            <h2 class="sm:text-3xl text-2xl title-font font-medium text-gray-900 mt-4 mb-4">Roof party normcore before they sold out, cornhole vape</h2>
-                            <p class="leading-relaxed mb-8">Live-edge letterpress cliche, salvia fanny pack humblebrag narwhal portland. VHS man braid palo santo hoodie brunch trust fund. Bitters hashtag waistcoat fashion axe chia unicorn. Plaid fixie chambray 90's, slow-carb etsy tumeric. Cray pug you probably haven't heard of them hexagon kickstarter craft beer pork chic.</p>
-                            <div class="flex items-center flex-wrap pb-4 mb-4 border-b-2 border-gray-100 mt-auto w-full">
-                                <a class="text-indigo-500 inline-flex items-center">Learn More
-                                    <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                        <path d="M5 12h14"></path>
-                                        <path d="M12 5l7 7-7 7"></path>
-                                    </svg>
-                                </a>
-                                <span class="text-gray-400 mr-3 inline-flex items-center ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
-                                    <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
-                                        <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
-                                        <circle cx="12" cy="12" r="3"></circle>
-                                    </svg>1.2K
-                                </span>
-                                <span class="text-gray-400 inline-flex items-center leading-none text-sm">
-                                    <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
-                                        <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                                    </svg>6
-                                </span>
+
+                    <div class="flex flex-wrap -m-4 text-center">
+                        <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
+                            <div class="border-2 border-gray-200 px-4 py-6 rounded-lg">
+                            <div className='flex justify-center'>
+                            <img  height={200}  width={200} src='/trophy 2.png' alt='gggg'/>
                             </div>
-                            <a class="inline-flex items-center">
-                                <img alt="blog" src="https://dummyimage.com/104x104" class="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center" />
-                                <span class="flex-grow flex flex-col pl-4">
-                                    <span class="title-font font-medium text-gray-900">Holden Caulfield</span>
-                                    <span class="text-gray-400 text-xs tracking-widest mt-0.5">UI DEVELOPER</span>
-                                </span>
-                            </a>
+                                <h2 class="title-font font-medium text-3xl text-white mt-3">2.7K Points</h2>
+                                <p class="mt-2 text-lg leading-relaxed text-white ">Silver Trophies</p>
+                            </div>
                         </div>
-                        <div class="p-12 md:w-1/2 flex flex-col items-start">
-                            <span class="inline-block py-1 px-2 rounded bg-indigo-50 text-indigo-500 text-xs font-medium tracking-widest">CATEGORY</span>
-                            <h2 class="sm:text-3xl text-2xl title-font font-medium text-gray-900 mt-4 mb-4">Pinterest DIY dreamcatcher gentrify single-origin coffee</h2>
-                            <p class="leading-relaxed mb-8">Live-edge letterpress cliche, salvia fanny pack humblebrag narwhal portland. VHS man braid palo santo hoodie brunch trust fund. Bitters hashtag waistcoat fashion axe chia unicorn. Plaid fixie chambray 90's, slow-carb etsy tumeric.</p>
-                            <div class="flex items-center flex-wrap pb-4 mb-4 border-b-2 border-gray-100 mt-auto w-full">
-                                <a class="text-indigo-500 inline-flex items-center">Learn More
-                                    <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                        <path d="M5 12h14"></path>
-                                        <path d="M12 5l7 7-7 7"></path>
-                                    </svg>
-                                </a>
-                                <span class="text-gray-400 mr-3 inline-flex items-center ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
-                                    <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
-                                        <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
-                                        <circle cx="12" cy="12" r="3"></circle>
-                                    </svg>1.2K
-                                </span>
-                                <span class="text-gray-400 inline-flex items-center leading-none text-sm">
-                                    <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
-                                        <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                                    </svg>6
-                                </span>
+                        <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
+                            <div class="border-2 border-gray-200 px-4 py-6 rounded-lg">
+                            <div className='flex justify-center'>
+                            <img  height={200}  width={200} src='/trophy.png' alt='gggg'/>
                             </div>
-                            <a class="inline-flex items-center">
-                                <img alt="blog" src="https://dummyimage.com/103x103" class="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center" />
-                                <span class="flex-grow flex flex-col pl-4">
-                                    <span class="title-font font-medium text-gray-900">Alper Kamu</span>
-                                    <span class="text-gray-400 text-xs tracking-widest mt-0.5">DESIGNER</span>
-                                </span>
-                            </a>
+                                <h2 class="title-font font-medium text-3xl text-white mt-3">1.3K Points</h2>
+                                <p class="mt-2 text-lg leading-relaxed text-white">Gold Trophies</p>
+                            </div>
+                        </div>
+                        <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
+                            <div class="border-2 border-gray-200 px-4 py-6 rounded-lg">
+                            <div className='flex justify-center'>
+                            <img  height={200}  width={200} src='/trophy 4.png' alt='gggg'/>
+                            </div>
+                                <h2 class="title-font font-medium text-3xl text-white mt-3">74 Points</h2>
+                                <p class="mt-2 text-lg leading-relaxed text-white">Rare Trophies</p>
+                            </div>
+                        </div>
+                        <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
+                            <div class="border-2 border-gray-200 px-4 py-6 rounded-lg">
+                            <div className='flex justify-center'>
+                            <img  height={200}  width={200} src='/trophy 3.png' alt='gggg'/>
+                            </div>
+                                <h2 class="title-font font-medium text-3xl text-white mt-3">46 Points</h2>
+                                <p class="mt-2 text-lg leading-relaxed text-white">Legendary Trophies</p>
+                            </div>
                         </div>
                     </div>
                 </div>
             </section>
+            </div>
         </>
     )
 }
