7d8715c812c95a8af4d9deb9767d6242b58dad0a
Ahzam Akhtar <ahzam8186@gmail.com>
2023-09-27 14:02:56 +0530

    HOME UI FI

--- components/micro/FriendsList.js
+++ components/micro/FriendsList.js
@@ -5,14 +5,17 @@ const FriendsList = () => {
     return (
         <>
             <div className="flex flex-col mt-20">
-
-                <MdExplore className='text-4xl' />
+                <div>
+                    <MdExplore className='text-4xl' />
+                </div>
                 <img class="w-24 h-8 mt-5 rounded-full" src="https://picsum.photos/200" alt="Rounded avatar"></img>
                 <img class="w-24 h-8 mt-5 rounded-full" src="https://picsum.photos/201" alt="Rounded avatar"></img>
                 <img class="w-24 h-8 mt-5 rounded-full" src="https://picsum.photos/202" alt="Rounded avatar"></img>
                 <img class="w-24 h-8 mt-5 rounded-full" src="https://picsum.photos/203" alt="Rounded avatar"></img>
                 <img class="w-24 h-8 mt-5 rounded-full" src="https://picsum.photos/204" alt="Rounded avatar"></img>
-                <AiOutlineUsergroupAdd className='text-4xl mt-24'/>
+                <div>
+                    <AiOutlineUsergroupAdd className='text-4xl mt-5' />
+                </div>
             </div>
         </>
     )
