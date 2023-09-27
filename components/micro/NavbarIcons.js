c0e8879c9c6b6a019991d72a7674c8ea055db003
Ahzam Akhtar <ahzam8186@gmail.com>
2023-09-27 21:15:59 +0530

    game bar added

--- components/micro/NavbarIcons.js
+++ components/micro/NavbarIcons.js
@@ -2,16 +2,37 @@ import React from 'react'
 import Image from 'next/image'
 const NavbarIcons = () => {
   return (
+    <>
 
-    <div className='flex flex-row '>
+      <div className='flex flex-row '>
+
+        <h2 className='font-abc text-2xl text-white mt-2'>
+          100
+        </h2>
+        <h2 className='font-abc text-2xl text-white mt-2 ml-2'>
+          XP
+        </h2>
+      </div>
+
+      
+      <div className='flex flex-row '>
+
+        <h2 className='font-abc text-2xl text-white mt-2'>
+          100 000
+        </h2>
+        <img class="w-12 h-12 ml-2 rounded-full" src="https://7fxmftuovhqylofjio4jlm2rrwdcmfrkz5hpgxiz25ffjjhskcvq.arweave.net/-W7Czo6p4YW4qUO4lbNRjYYmFirPTvNdGddKVKTyUKs" alt="Rounded avatar"></img>
+      </div>
+
+
+
 
       <h2 className='font-abc text-2xl text-white mt-2'>
         UserName
       </h2>
-      <img class="w-10 h-10 ml-5 rounded-full" src="https://picsum.photos/200/300" alt="Rounded avatar"></img>
-    </div>
+      <img class="w-10 h-10 ml-2 rounded-full" src="https://picsum.photos/200/300" alt="Rounded avatar"></img>
 
 
+    </>
   )
 }
 
--- pages/Games/[slug].js
+++ pages/Games/[slug].js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import Coinflip from '../../components/games/Coinflip';
+import { BiSolidUpArrow } from "react-icons/bi"
 const Games = ({ slug }) => {
     const [show, setshow] = useState(true)
 
@@ -28,19 +29,69 @@ const Games = ({ slug }) => {
                 <div class="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
                     <div class="lg:w-2/3 md:w-1/2 rounded-lg overflow-hidden sm:mr-10 ">
                         <div className='flex justify-center'>
-                            <div className='flex flex-row'>
-                                <h1 className='text-white font-abc text-6xl mt-5'>{slug}</h1>
-                            </div>
-                        </div>
-                        <div className='flex justify-center p-2'>
-                        <button class="py-3 text-black font-abc bg-white border-0  px-6 m-2 focus:outline-none rounded text-2xl"
-                        onClick={()=>setshow(!show)}
-                        >Play</button>
-                        </div>
-                        <div className='ml-16'>
-                            {show ? (
 
-                                <img height={600} width={900} src='/gamethu1.jpg' alt='gggg' />
+                        </div>
+                        <section class="text-gray-400 bg-gray-900 body-font rounded-xl">
+                            <div class="container px-5 py-10 mx-auto">
+                                <div class="flex flex-wrap -m-4 text-center">
+                                    <div class="p-4 sm:w-1/4 w-1/2">
+                                        <h2 class="font-abc title-font font-medium sm:text-4xl text-3xl text-white">CoinFlip</h2>
+                                        <p class="text-white font-abc text-3xl">Game</p>
+                                    </div>
+                                    <div class="p-4 sm:w-1/4 w-1/2">
+
+                                        <div className='flex'>
+                                            <a class="inline-flex items-center">
+                                                <BiSolidUpArrow className='text-green-500 text-3xl' />
+                                                <span class="flex-grow flex flex-col pl-3">
+                                                    <span class="text-4xl font-abc text-white">100+ XP</span>
+                                                </span>
+                                            </a>
+
+
+                                        </div>
+                                    </div>
+                                    <div class="p-4 sm:w-1/4 w-1/2">
+
+                                        <div className='flex'>
+                                            <a class="inline-flex items-center">
+                                                <img src='/cade.png' height={50} width={50} alt='cade' />
+                                                <span class="flex-grow flex flex-col pl-3">
+                                                    <span class="text-4xl font-abc text-white">100+ XP</span>
+                                                </span>
+                                            </a>
+
+
+                                        </div>
+
+                                    </div>
+                                    <div class="p-4 sm:w-1/4 w-1/2">
+                                        <h2 class="font-abc sm:text-4xl text-3xl text-white">400+</h2>
+                                        <p class="text-white font-abc text-3xl">Times Played</p>
+                                    </div>
+                                    <div className='ml-16 '>
+                                    <button class="py-3 text-black font-abc bg-white border-0  px-6 m-2 focus:outline-none rounded text-2xl"
+                                        onClick={() => setshow(!show)}
+                                    >Play Now</button>
+                                    </div>
+                                    <div className='ml-16 '>
+                                    <button class="py-3 text-black font-abc bg-white border-0  px-6 m-2 focus:outline-none rounded text-2xl"
+                                        onClick={() => setshow(!show)}
+                                    >Claim XP and Cade</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                        {/* <div className='flex justify-center p-2'>
+                            <button class="py-3 text-black font-abc bg-white border-0  px-6 m-2 focus:outline-none rounded text-2xl"
+                                onClick={() => setshow(!show)}
+                            >Play</button>
+                        </div> */}
+                        <div className=''>
+                            {show ? (
+                                <div className='p-10'>
+                                    <img width={800} src='/gamethu1.jpg' alt='gggg' />
+                                </div>
 
                             ) : (
                                 <Coinflip />
