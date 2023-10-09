5e3da96764c91e968672164d4f01a7ac50a76f04
Nat <natalie.catherine.yeo@gmail.com>
2023-10-10 04:46:10 +0800

    add UI for landing page tower defence

--- components/games/TowerDefence.js
+++ components/games/TowerDefence.js
@@ -1,9 +1,32 @@
 import React from "react";
+import { Button } from "react-scroll";
 
 const TowerDefence = () => {
   return (
     <>
-      <div className="flex justify-center p-16 bg-black"></div>
+      <>
+        <div className=" p-16 bg-gray-900">
+          <div className="flex justify-center">
+            <h1 className="text-white font-abc text-5xl mt-5">Solana Royale</h1>
+          </div>
+          <div className="mt-20">
+            <div class="flex items-center justify-between">
+              <button
+                type="submit"
+                className="inline-flex items-centre py-2.5 px-5 mx-20 text-sm font-large text-white bg-lime-500 outline outline-lime-500 border-lime-600 hover:bg-lime-600 focus:ring-4 focus:outline-none focus:ring-lime-600  dark:bg-lime-600 dark:hover:bg-lime-600 dark:focus:ring-lime-600 w-54 h-18 "
+              >
+                Create a New Map
+              </button>
+              <button
+                type="submit"
+                class="inline-flex py-2.5 px-5 mx-20 text-sm font-large text-white bg-lime-500 border-lime-500 outline outline-lime-500 border-lime-600 hover:bg-lime-600 focus:ring-4 focus:outline-none focus:ring-lime-600  dark:bg-lime-600 dark:hover:bg-lime-600 dark:focus:ring-lime-600 w-54 h-18 "
+              >
+                Select a Map
+              </button>
+            </div>
+          </div>
+        </div>
+      </>
     </>
   );
 };
