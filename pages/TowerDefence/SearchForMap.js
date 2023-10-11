311f7f445d06ea30e9302055e7c59e2eebf25484
Nat <natalie.catherine.yeo@gmail.com>
2023-10-11 23:18:46 +0800

    added code

--- pages/TowerDefence/SearchForMap.js
+++ pages/TowerDefence/SearchForMap.js
@@ -1,7 +1,39 @@
-const SearchForMap = () => {
-  return (
-    <>
-      <div className="flex justify-center p-16 bg-black"></div>
-    </>
-  );
-};
+import * as anchor from "@coral-xyz/anchor";
+import { useEffect, useMemo, useState } from "react";
+import { TOWER_DEFENCE_PROGRAM_ID } from "../constant/index";
+import { IDL } from "../constant/towerdefence";
+
+import {
+  PublicKey,
+  SYSVAR_RENT_PUBKEY,
+  SystemProgram,
+  Transaction,
+} from "@solana/web3.js";
+import { utf8 } from "@coral-xyz/anchor/dist/cjs/utils/bytes";
+import {
+  useAnchorWallet,
+  useConnection,
+  useWallet,
+} from "@solana/wallet-adapter-react";
+
+export function useFourInLine() {
+  const { connection } = useConnection();
+  const { publicKey } = useWallet();
+  const anchorWallet = useAnchorWallet();
+
+  const program = useMemo(() => {
+    if (anchorWallet) {
+      const provider = new anchor.AnchorProvider(
+        connection,
+        anchorWallet,
+        anchor.AnchorProvider.defaultOptions()
+      );
+      return new anchor.Program(IDL, FOUR_IN_LINE_PROGRAM_ID, provider);
+    }
+  }, [connection, anchorWallet]);
+  return {
+    program,
+  };
+}
+
+export const CreateGame = () => {};
