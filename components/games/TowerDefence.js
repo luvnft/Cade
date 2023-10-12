95b5d9b4746355afc3e2bf1dafc301e18dbc3fa0
Nat <natalie.catherine.yeo@gmail.com>
2023-10-12 17:24:39 +0800

    add mobile responsiveness and UI for CreateMap

--- components/games/TowerDefence.js
+++ components/games/TowerDefence.js
@@ -1,28 +1,39 @@
 import React from "react";
 import { Button } from "react-scroll";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 const TowerDefence = () => {
+  const router = useRouter();
   return (
     <>
       <>
-        <div className=" p-16 bg-gray-900">
-          <div className="flex justify-center">
-            <h1 className="text-white font-abc text-5xl mt-5">Solana Royale</h1>
+        <div className=" p-4 bg-gray-900">
+          <div className="flex flex-col items-center">
+            <h1 className="text-white font-abc text-3xl mt-5 text-center">
+              Solana Royale
+            </h1>
           </div>
-          <div className="mt-20">
-            <div class="flex items-center justify-between">
-              <button
-                type="submit"
-                className="inline-flex items-centre py-2.5 px-5 mx-20 text-sm font-large text-white bg-lime-500 outline outline-lime-500 border-lime-600 hover:bg-lime-600 focus:ring-4 focus:outline-none focus:ring-lime-600  dark:bg-lime-600 dark:hover:bg-lime-600 dark:focus:ring-lime-600 w-54 h-18 "
-              >
-                Create a New Map
-              </button>
-              <button
-                type="submit"
-                class="inline-flex py-2.5 px-5 mx-20 text-sm font-large text-white bg-lime-500 border-lime-500 outline outline-lime-500 border-lime-600 hover:bg-lime-600 focus:ring-4 focus:outline-none focus:ring-lime-600  dark:bg-lime-600 dark:hover:bg-lime-600 dark:focus:ring-lime-600 w-54 h-18 "
-              >
-                Select a Map
-              </button>
+          <div className="mt-4">
+            <div className="flex flex-col items-center sm:flex-row sm:items-center">
+              <div className="mt-4 items-start sm:mt-0 mx-20">
+                <Link href="./../pages/TowerDefence/CreateMap.js">
+                  <button
+                    type="submit"
+                    className="inline-flex items-center py-2.5 px-5 mx-20 text-sm font-large text-white bg-lime-500 outline outline-lime-500 border-lime-600 hover:bg-lime-600 focus:ring-4 focus:outline-none focus:ring-lime-600 dark:bg-lime-600 dark:hover:bg-lime-600 dark:focus:ring-lime-600"
+                  >
+                    Create a New Map
+                  </button>
+                </Link>
+              </div>
+              <div className="mt-4 sm:mt-0 mx-20">
+                <button
+                  type="submit"
+                  class="inline-flex py-2.5 px-5 mx-20 text-sm font-large text-white bg-lime-500 border-lime-500 outline outline-lime-500 border-lime-600 hover:bg-lime-600 focus:ring-4 focus:outline-none focus:ring-lime-600 dark:bg-lime-600 dark:hover:bg-lime-600 dark:focus:ring-lime-600"
+                >
+                  Select a Map
+                </button>
+              </div>
             </div>
           </div>
         </div>
--- pages/TowerDefence/CreateMap.js
+++ pages/TowerDefence/CreateMap.js
@@ -1,41 +1,41 @@
-import {  useState } from "react";
+import { useState } from "react";
 import { useAnchorWallet, useConnection } from "@solana/wallet-adapter-react";
 import Head from "next/head";
 import { useProgram } from "../../connector/ddt-utils/useProgram";
 
-import {
-  createMap
-} from '../../connector/ddt-utils/callInstructions'
+import { createMap } from "../../connector/ddt-utils/callInstructions";
 
 const CreateMap = (props) => {
   const wallet = useAnchorWallet();
   const { connection } = useConnection();
   const { program } = useProgram({ connection, wallet });
 
-// React UseStates hooks for managing args 
-//for createMap
-const [name_for_createMap , setname_for_createMap] = useState()
-const [board_for_createMap , setboard_for_createMap] = useState()
-const [budget_for_createMap , setbudget_for_createMap] = useState()
-const [board , setboard] = useState([{
-  kind: "tree",
-  health: 1,
-  dps: 1,
-  position: 0
-},])
-//handler functions for inputs feilds
-const namehandler_for_createMap = (e) => {
-  setname_for_createMap(e.target.value)
-}
-const boardhandler_for_createMap = (e) => {
-  setboard_for_createMap(e.target.value)
-}
-const budgethandler_for_createMap = (e) => {
-  setbudget_for_createMap(e.target.value)
-}
-const deployshandler_for_deployUnits = (e) => {
-  setdeploys_for_deployUnits(e.target.value)
-}
+  // React UseStates hooks for managing args
+  //for createMap
+  const [name_for_createMap, setname_for_createMap] = useState();
+  const [board_for_createMap, setboard_for_createMap] = useState();
+  const [budget_for_createMap, setbudget_for_createMap] = useState();
+  const [board, setboard] = useState([
+    {
+      kind: "tree",
+      health: 1,
+      dps: 1,
+      position: 0,
+    },
+  ]);
+  //handler functions for inputs feilds
+  const namehandler_for_createMap = (e) => {
+    setname_for_createMap(e.target.value);
+  };
+  const boardhandler_for_createMap = (e) => {
+    setboard_for_createMap(e.target.value);
+  };
+  const budgethandler_for_createMap = (e) => {
+    setbudget_for_createMap(e.target.value);
+  };
+  const deployshandler_for_deployUnits = (e) => {
+    setdeploys_for_deployUnits(e.target.value);
+  };
 
   return (
     <>
@@ -43,94 +43,60 @@ const deployshandler_for_deployUnits = (e) => {
         <title>dtt</title>
         <meta name="description" content="dtt" />
       </Head>
-       <div className="flex justify-center py-10">
-        <h1 className="sm:text-3xl text-2xl font-extrabold mb-4 text-white">
-          Call Instructions of Your IDL By a Clicking a Button
-        </h1>
-      </div>
-      <div className="flex justify-center">
-        <h1 className="sm:text-3xl text-2xl font-extrabold mb-4 text-white">
-          Powered By SODA
-        </h1>
-      </div>
-    <div className="text-white flex flex-col text-2xl m-5 p-2 ">
-        <div className="min-h-screen bg-inherit py-1 flex flex-col justify-center sm:py-12">
-          <div className="relative py-3 sm:max-w-xl sm:mx-auto">
-            <div className="relative px-4 py-10 bg-white mx-8 md:mx-0 shadow rounded-3xl sm:p-10">
-              <div className=" mx-auto">
-                <div className="flex items-center space-x-5">
-                  <div className="h-16 w-16 p-2 bg-black rounded-full flex flex-shrink-0 justify-center items-center text-3xl font-mono">
-                    <img
-                      src="https://firebasestorage.googleapis.com/v0/b/shrine-76128.appspot.com/o/soda.png?alt=media&token=32de0266-c1ee-4a31-a2e1-2df6d35086f8"
-                      alt="ff"
-                    />
-                  </div>
-                  <div className="block pl-2 font-semibold text-xl self-start text-gray-700">
-                    <h2 className="leading-relaxed mt-8">For createMap instructions</h2>
-                    <p className="text-sm text-gray-500 font-normal leading-relaxed invisible">
-                      Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                    </p>
-                  </div>
-                </div>
-                <div className="divide-y divide-gray-200">
-                  <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
-                    <div className="flex flex-col">
-                      <p className="">name</p>
-                      <input
-                        type="text"
-                        className="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600"
-                        placeholder={`Enter name`}
-                        value={
-                          name_for_createMap
-                        }
-                        onChange={
-                          namehandler_for_createMap
-                        }
-                      />
-                    </div>
-                    <div className="flex flex-col">
-                      <p className="">board</p>
-                      <input
-                        type="text"
-                        className="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600"
-                        placeholder={`Enter board`}
-                        value={
-                          board_for_createMap
-                        }
-                        onChange={
-                          boardhandler_for_createMap
-                        }
-                      />
-                    </div>
-                    <div className="flex flex-col">
-                      <p className="">budget</p>
-                      <input
-                        type="text"
-                        className="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600"
-                        placeholder={`Enter budget`}
-                        value={
-                          budget_for_createMap
-                        }
-                        onChange={
-                          budgethandler_for_createMap
-                        }
-                      />
-                    </div>
-                  </div>
-                  <div className="pt-4 flex items-center space-x-4">
-                    
-                    <button 
-                    className="bg-blue-500 flex justify-center items-center w-full text-white px-3 text-xl py-1 rounded-md focus:outline-none"
-                    onClick={
-                      ()=>createMap(program , name_for_createMap , board , budget_for_createMap, wallet.publicKey,)
-                    }
-                    >
-                       Call_createMap_instruction
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
+      <div className=" p-200 bg-gray-900">
+        <div className="font-semibold text-m text-white font-abc text-center">
+          <h2>Instructions to Create a New Map</h2>
+          <p className="text-sm text-white font-normal leading-relaxed mt-2">
+            Please fill in the Name of the Map, Type of Board and available
+            Budget for Units.
+          </p>
+        </div>
+        <div className="px-2 mt-2 text-gray-200 sm:text-lg sm:leading-2 flex flex-col items-center">
+          <div className="flex flex-col w-60 items-center">
+            <p className="">Board Name</p>
+            <input
+              type="text"
+              className="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600"
+              placeholder={`Enter name`}
+              value={name_for_createMap}
+              onChange={namehandler_for_createMap}
+            />
+          </div>
+          <div className="flex flex-col w-60 items-center">
+            <p className="">Type of Board</p>
+            <input
+              type="text"
+              className="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600"
+              placeholder={`Enter board`}
+              value={board_for_createMap}
+              onChange={boardhandler_for_createMap}
+            />
+          </div>
+          <div className="flex flex-col w-60 items-center">
+            <p className="">Budget for units</p>
+            <input
+              type="text"
+              className="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600"
+              placeholder={`Enter budget`}
+              value={budget_for_createMap}
+              onChange={budgethandler_for_createMap}
+            />
+          </div>
+          <div className="pt-4 flex flex-col items-center space-x-4">
+            <button
+              className="bg-blue-500 flex  justify-center items-center text-white py-2.5 px-5 text-xl rounded-md focus:outline-none"
+              onClick={() =>
+                createMap(
+                  program,
+                  name_for_createMap,
+                  board,
+                  budget_for_createMap,
+                  wallet.publicKey
+                )
+              }
+            >
+              Create Map
+            </button>
           </div>
         </div>
       </div>
