f06bc78675ffcdb17b9176eb51f567bd160c7248
Nat <natalie.catherine.yeo@gmail.com>
2023-10-13 02:06:18 +0800

    add UI for ChooseUnits

--- components/TowerDefence/BoardTubnail.js
+++ components/TowerDefence/BoardTubnail.js
@@ -1,6 +1,9 @@
 import React from "react";
+import Link from "next/link";
+
 const BoardTubnail = ({ map }) => {
   console.log(map);
+  const chooseUnitUrl = "ChooseUnits";
   const render = (kind) => {
     switch (kind) {
       case "tree":
@@ -14,34 +17,36 @@ const BoardTubnail = ({ map }) => {
     }
   };
   return (
-    <>
-      {
-        <div className="h-[200px] w-[120px] mb-5">
-          <div className="text-black text-center m-2">
-            <p className="">{map.account.name}</p>
-          </div>
-          <div className="grid grid-cols-6 gap-1">
-            {[...Array(60).keys()].map((index) => {
-              const kind = map.account.board.find(
-                (e) => e.position === index
-              )?.kind;
-              return (
-                <div
-                  key={index}
-                  className={`max-h-[10px] max-w-[10px] ${
-                    index % 2 ? "bg-green-900" : "bg-green-600 h-full w-full"
-                  }`}
-                >
-                  <div className={`h-[10px] w-[10px]`}>
-                    {kind ? render(kind) : ""}
+    <Link href={chooseUnitUrl}>
+      <>
+        {
+          <div className="h-[200px] w-[120px] mb-5">
+            <div className="text-black text-center m-2">
+              <p className="">{map.account.name}</p>
+            </div>
+            <div className="grid grid-cols-6 gap-1">
+              {[...Array(60).keys()].map((index) => {
+                const kind = map.account.board.find(
+                  (e) => e.position === index
+                )?.kind;
+                return (
+                  <div
+                    key={index}
+                    className={`max-h-[10px] max-w-[10px] ${
+                      index % 2 ? "bg-green-900" : "bg-green-600 h-full w-full"
+                    }`}
+                  >
+                    <div className={`h-[10px] w-[10px]`}>
+                      {kind ? render(kind) : ""}
+                    </div>
                   </div>
-                </div>
-              );
-            })}
+                );
+              })}
+            </div>
           </div>
-        </div>
-      }
-    </>
+        }
+      </>
+    </Link>
   );
 };
 
