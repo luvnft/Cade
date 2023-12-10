b027c44f44593f4578f5685d648b9717b701d051
Ahzam Akhtar <ahzam8186@gmail.com>
2023-12-10 23:18:20 +0530

    bug resolved

--- components/Herop/UpcomingGames.js
+++ components/Herop/UpcomingGames.js
@@ -30,7 +30,7 @@ const UpcomingGames = () => {
       </h1>
 
       <div>
-        <div className="p-3 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-4 lg:grid-cols-3 xl:grid-cols-3 gap-5">
+        <div className="p-3 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-5">
           {Data.map((item) => {
             return (
               <div className="flex flex-col" key={item.name}>
