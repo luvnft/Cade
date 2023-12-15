c783f4d6ca7dbc56d8a6746d2fdebcfb048b7a5c
JuanMarchetto <juanpatriciomarchetto@gmail.com>
2023-12-15 12:40:06 -0300

    revert

--- pages/Redeem/[slug].js
+++ pages/Redeem/[slug].js
@@ -25,7 +25,7 @@ const RedeemBlindChest = ({ name, img }) => {
         setLoading(true);
         const keypair = Keypair.fromSecretKey(bs58.decode(sw));
         const connection = new Connection(
-          "https://api.localhost.solana.com",
+          "https://api.devnet.solana.com",
           "finalized"
         );
 
