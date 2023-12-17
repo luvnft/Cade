0dab6df01afcf6f3c9ad66452dfaa00c2edf6778
Marche <juanpatriciomarchetto@gmail.com>
2023-12-17 18:05:22 -0300

    Update based on events (#27)

--- pages/bonk-or-death/index.js
+++ pages/bonk-or-death/index.js
@@ -1,7 +1,8 @@
 import { FC, useEffect, useState } from "react";
 import { useAnchorWallet, useConnection } from "@solana/wallet-adapter-react";
 import Head from "next/head";
-import { useProgram } from "../../connector/bonk-or-death/useProgram";
+//import { useProgram } from "../../connector/bonk-or-death/useProgram";
+import { useProgram } from "../../connector/cumulative_leaderboard.ts/useProgram";
 import { useRouter } from "next/router";
 import { Keypair, PublicKey } from "@solana/web3.js";
 import * as web3 from "@solana/web3.js";
@@ -17,21 +18,48 @@ const ChooseUnits = (props) => {
   const { program } = useProgram({ connection, wallet });
 
   let seed = new BN(randomBytes(16));
-
+  
   return (
     <>
       <Head>
         <title>{`'Cade BONK or DEATH`}</title>
         <meta name="description" content="BONK or DEATH" />
       </Head>
-      <section className="text-gßray-600 body-font relative bg-[url('/kn.jpg')]">
+      <section className="text-gßray-600 body-font relative bg-[url('/kn.jpg')] min-h-screen">
         <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
-          <div className="lg:w-2/3 md:w-1/2 rounded-lg overflow-hidden sm:mr-10 ">
-            <div className="flex justify-center">
+        <h1 className="text-center w-full">DETH is temporary, Solana is inevitable</h1>
+        </div>
+        <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
+          <div className=" w-full rounded-lg overflow-hidden ">
+            <div className="flex justify-center items-center min-h-[400]px">
               {/*<button onClick={() => placeBet(program, seed, wallet)}>place</button>
               <button onClick={() => resolveBet(program, seed, wallet)}>resolve</button>*/}
               <Dice
-                onRoll={(value) => console.log(value)}
+                onRoll={(value) => {
+                  (async()=>{ if (program) {
+                    try {
+                      const [global] = web3.PublicKey.findProgramAddressSync(
+                        [Buffer.from("leaderboard")],
+                        program.programId,
+                      )
+                      const [user] = web3.PublicKey.findProgramAddressSync(
+                        [Buffer.from("user"), global.toBuffer(), wallet.publicKey.toBuffer()],
+                        program.programId,
+                      )
+                      const tx = await program.methods
+                        .play()
+                        .accounts({ user, global, payer: wallet.publicKey })
+                        .rpc({
+                          skipPreflight:true
+                        })  ;
+                  
+                      console.log("Your transaction signature is: ", tx);
+                      } catch (e) {
+                        console.log(e);
+                      }
+                  }})()
+                  console.log(value)
+                }}
                 faces={["bonk.jpg", "death.jpeg", "death.jpeg", "death.jpeg", "death.jpeg", "death.jpeg"]}
               />
             </div>
